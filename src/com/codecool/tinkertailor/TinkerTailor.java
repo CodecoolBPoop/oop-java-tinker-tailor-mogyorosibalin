d566f3395d1966241504735be695313e165c7cc5
mogyorosibalin <mogyorosibalin@gmail.com>
2018-06-23 22:19:31 +0200

    Assignment ready

---
+++ src/com/codecool/tinkertailor/TinkerTailor.java
@@ -0,0 +1,46 @@
+package com.codecool.tinkertailor;
+
+import java.util.ArrayList;
+
+public class TinkerTailor {
+    private static int baseN = 5;
+    private ArrayList<Integer> players = new ArrayList<>();
+
+    private TinkerTailor() {
+        resetPlayers(baseN);
+    }
+
+    private TinkerTailor(int n) {
+        resetPlayers(n);
+    }
+
+    private void resetPlayers(int n) {
+        for (int i = 0; i < n; i++) {
+            players.add(i + 1);
+        }
+    }
+
+    private int size() {
+        return players.size();
+    }
+
+    private ArrayList<Integer> game(int k) {
+        ArrayList<Integer> sequence = new ArrayList<>(size());
+        ArrayList<Integer> tempPlayers = players;
+        int startIx = 0;
+        int out;
+        while (tempPlayers.size() != 0) {
+            out = (startIx + k - 1) % tempPlayers.size();
+            sequence.add(tempPlayers.get(out));
+            tempPlayers.remove(out);
+            startIx = out;
+        }
+        return sequence;
+    }
+
+    public static void main(String[] args) {
+        TinkerTailor t = new TinkerTailor();
+        ArrayList<Integer> result = t.game(3);
+        System.out.println(result.toString());
+    }
+}
